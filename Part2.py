d1bd276efac2a8b449334336873326bdca403623
Yuriy Bandura <bandurayv@gmail.com>
2024-10-02 21:25:14 +0300

    Add_5

---
+++ Part2.py
@@ -0,0 +1,20 @@
+from calendar import month
+
+import pandas as pd
+import numpy as np
+
+dates = pd.date_range('2022-07-26', periods=10, freq='D')
+
+values = np.random.rand(10)
+
+df = pd.DataFrame({'Date': dates, 'Values': values})
+
+df.set_index('Date', inplace=True)
+
+print(df)
+
+month = df.resample('M').mean()
+
+print(month)
+
+
